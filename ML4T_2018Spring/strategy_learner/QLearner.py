d47356b6f820df7fa06bb2539513a7eaf063c0f1
Melvin Dunn <dunn_melvin@bah.com>
2018-08-03 00:01:01 -0400

    Add files via upload

---
+++ ML4T_2018Spring/strategy_learner/QLearner.py
@@ -0,0 +1,167 @@
+"""
+Template for implementing QLearner  (c) 2015 Tucker Balch
+
+
+"""
+
+import numpy as np
+import random as rand
+
+class QLearner(object):
+
+    """
+    alpha - how much do you trust new information
+    gamma - what is the value of future rewards
+
+    until you reach the goal, all rewards are negative.
+
+    essentially because it's getting a negative reward,
+    your robot wants to get to the goal immediately.
+
+    State of objective for the learner is to find a policy that
+    maximizes reward. In the fewest amount of steps as possible.
+
+    rar, random action rate - if you look at the book, or one of
+    the original q-learning papers, early on it should choose random actions.
+    if you don't do that, you won't force the learner to explore.
+
+    radr - random action decay rate.
+
+    if you take a look at test_qlearner.py you'll see the numbers for radr and rar
+
+    dyna is set to zero, for dyna the number of dyna updates that you do.
+    you'll need to update that when you implement dyna.
+
+    verbose is just going to be false.
+
+    query is the primary function that implements q-learning
+    the way it works is we call a test harness.
+    This is the new state you're in, sprime, and this is the reward
+    you get for the last step you took.
+
+    The steps you need to folow within this funciton, 1st update the q-table.
+
+    you're in a new state and you're getting a reward for the next action you took.
+
+    what was the state I was in before, what action you took beflow
+
+    experience tuple is s - the state you're in, an experience touple, (s, a, s_prime, r)
+
+    query - update the q table, rollymcdice to see if you take a random action. update rar
+    but what if you don't take a random action?
+
+    what action should I take?
+
+    remember your q-table answers that problem for you.
+
+    you look at the q value for each of the actions you might take.
+
+    there's one other function, querysetstate -
+    you need to remember the last state you were in
+    the last action you took. If this is the very first action, you can't
+    remember it.
+
+    just remember that was the state you were in
+
+    that is the whole api that you should implement.
+
+    you are supposed to pick a random action with probablity rar
+
+    what about for the actual trader?
+
+    Whe're going to predifine 3 possible actions.
+
+    A lot of success in applying machine learning to a problem involves mapping the problem it solves to the problem that the learner can solve.
+
+    you need to consider what the algorithm can do and map your problem to that.
+
+    how high resolution do you want to consider oyur factors?
+
+    discretizing your factors into an integer so you can plugin your q-learner.
+    """
+    def __init__(self, \
+        num_states=100, \
+        num_actions = 4, \
+        alpha = 0.2, \
+        gamma = 0.9, \
+        rar = 0.5, \
+        radr = 0.99, \
+        dyna = 0, \
+        verbose = True):
+
+        self.verbose = verbose
+        self.num_actions = num_actions
+        self.num_states = num_states
+        self.q_table = np.random.uniform(-1.0,1.0,size=(num_states,num_actions)) #np.zeros((num_states, num_actions))
+        self.rar = rar
+        self.radr = radr
+        self.alpha = alpha
+        self.gamma = gamma
+        self.s = rand.randint(0, num_states-1)
+        self.a = 0
+        self.dyna = dyna
+        if self.dyna is not 0:
+        	self.T_count = np.zeros((num_states, num_actions, num_states)) + 0.00001
+        	self.T = self.T_count / self.T_count.sum(axis=2, keepdims=True)
+        	self.R = -1 * np.ones((num_states, num_actions))
+
+
+    def author(self):
+        return "mdunn34"
+
+    def querysetstate(self, s):
+        """
+        @summary: Update the state without updating the Q-table
+        @param s: The new state
+        @returns: The selected action
+        """
+        self.s = s
+        action = np.argmax(self.q_table[self.s, :])
+        if (rand.uniform(0,1)) > self.rar:
+            action = np.argmax(self.q_table[self.s,])
+        if self.verbose: print "s =", s,"a =",action
+        return action
+
+    def query(self,s_prime,r):
+        """
+        @summary: Update the Q table and return an action
+        @param s_prime: The new state
+        @param r: The new reward
+        @returns: The selected action
+        """
+        old_value = ((1. - self.alpha) * self.q_table[self.s, self.a])
+        improved_estimate = self.alpha * (r + self.gamma * \
+            self.q_table[s_prime, np.argmax(self.q_table[s_prime, :])])
+        update = old_value + improved_estimate
+        self.q_table[self.s, self.a] = update
+
+        if rand.random() <= self.rar:
+        	action = rand.randint(0, self.num_actions-1)
+        else:
+        	action = np.argmax(self.q_table[s_prime,:])
+
+        self.rar -= self.radr
+
+        if self.dyna is not 0:
+        	# T' update and R update
+        	self.T_count[self.s, self.a, s_prime] += 1
+        	self.T = self.T_count / self.T_count.sum(axis=2, keepdims=True)
+        	self.R[self.s, self.a] = (1-self.alpha) * self.R[self.s, self.a] + (self.alpha * r)
+        	for i in range(0, self.dyna):
+	        	S = rand.randint(0, self.num_states-1)
+	        	A = rand.randint(0, self.num_actions-1)
+	        	S_prime = np.argmax(self.T_count[S, A, :])
+	        	r = self.R[S, A]
+	        	old_dyna_value = ((1. - self.alpha) * self.q_table[S, A])
+		        improved_dyna_estimate = self.alpha * (r + self.gamma * \
+		            self.q_table[S_prime, np.argmax(self.q_table[S_prime, :])])
+		        dyna_update = old_dyna_value + improved_dyna_estimate
+		        self.q_table[S, A] = dyna_update
+
+        self.a = action
+        self.s = s_prime
+        if self.verbose: print "s =", s_prime,"a =",action,"r =",r
+        return action
+
+if __name__=="__main__":
+    print "Remember Q from Star Trek? Well, this isn't him"
--- ML4T_2018Spring/strategy_learner/StrategyLearner.py
+++ ML4T_2018Spring/strategy_learner/StrategyLearner.py
@@ -6,19 +6,149 @@ import datetime as dt
 import pandas as pd
 import util as ut
 import random
+import numpy as np
+from QLearner import QLearner
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 class StrategyLearner(object):
 
     # constructor
-    def __init__(self, verbose = False, impact=0.0):
+    def __init__(self, verbose = False, impact=0.0, save_data=True):
         self.verbose = verbose
         self.impact = impact
+        self.nbins = 5
+        self.save_data = save_data
+    def author(self):
+        return "mdunn34"
+
+
+    def bollinger(self, prices, window_n=20, k=2):
+        std = prices.rolling(window=window_n).std()
+        mean = prices.rolling(window=window_n).mean()
+        upper_bound = mean + (k * std)
+        lower_bound = mean - (k * std)
+        band = (prices-lower_bound)/(upper_bound-lower_bound)
+        normed_band = (band-(band).mean())/(band.std())
+        bandwidth = (upper_bound - lower_bound) / mean * 100
+        normed_band = normed_band.fillna(method="bfill")
+        bandwidth = bandwidth.fillna(method="bfill")
+        return normed_band, bandwidth
+
+    def add_indicators(self, prices, window_n=5, n_bins=5, save_data=False, symb=None):
+        n_bins = self.nbins
+        prices_new = prices.copy()
+
+        def plot_data(df, type, title="Stock prices", xlabel="Date", ylabel="Value", symb=None):
+            """Plot stock prices with a custom title and meaningful axis labels."""
+            ax = df.plot(title=title, fontsize=12)
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            plt.savefig('{}_{}.png'.format(type, str(symb)))
+
+        for symb in prices.columns:
+            #bollinger bands here.
+            bolli, bandwidth = self.bollinger(prices[symb], window_n=window_n)
+
+            momentum = ((prices[symb] / prices[symb].shift(-1)) * 100).fillna(method="ffill")
+
+
+            # rsi
+            difference = prices[symb].diff()
+            diff_up, diff_down = difference.copy(), difference.copy()
+            diff_up[diff_up < 0] = 0
+            diff_down[diff_down > 0] = 0
+            rsi =  100. - (100. / (1. + (diff_up.rolling(window=window_n).mean() / diff_down.rolling(window=window_n).mean().abs())))
+            rsi = rsi.fillna(method="bfill")
+
+            if save_data == True:
+                print("Correlation between RSI and Momentum", np.corrcoef(rsi, momentum))
+
+                plot_data(bandwidth, type='bandwidth', title='Bandwidth Values')
+                plt.clf()
+                plot_data(bolli, type='bollinger', title='Normed Bollinger Values')
+                plt.clf()
+                plot_data(momentum, type='momentum', title='Momentum Values')
+                plt.clf()
+                plot_data(rsi, type='rsi', title='RSI Values')
+                plt.clf()
+
+            bolli = pd.cut(bolli, bins=n_bins, labels=False)
+            bandwidth = pd.cut(bandwidth, bins=n_bins, labels=False)
+            momentum = pd.cut(momentum, bins=n_bins, labels=False)
+            rsi = pd.cut(rsi, bins=n_bins, labels=False)
+        return np.asarray(bolli), np.asarray(bandwidth), np.asarray(momentum), np.asarray(rsi)
+
+    def discretize(self, state_bolli, state_bandwidth, state_momentum, state_rsi):
+        return (state_bolli) + (state_bandwidth ** 2) + (state_momentum ** 3) + (state_rsi ** 4)
+
+    def init_trades_and_prices_df(self, prices):
+        prices_dataframe = prices.copy()
+        prices_dataframe['Cash'] = np.ones(prices.shape[0])
+        df_trades = pd.DataFrame(np.zeros(prices_dataframe.shape), index=prices_dataframe.index, columns=prices_dataframe.columns)
+        return prices_dataframe, df_trades
+
+    def compute_last_reward(self, prices, holdings, action, i):
+        if i == 0:
+            reward = 0
+        else:
+            # compute the daily return.
+            daily_return = ((prices.iloc[i] - prices.iloc[i - 1]) / prices.iloc[i]) * 100
+            if holdings != 0:
+                # Sell
+                if action == 0:
+                    reward = -1.0 * daily_return
+                # Buy
+                elif action == 1:
+                    reward = daily_return
+                else:
+                    reward = -9
+            else:
+                reward = 0
+        return reward
+
+    def compute_trades_and_holdings_buy(self, df_trades, prices_dataframe, symbol, holdings, diggity_day):
+        if holdings == -1000 :
+            df_trades["Cash"].loc[diggity_day] = df_trades["Cash"].loc[diggity_day] + 2000. * prices_dataframe[symbol].loc[diggity_day] * -1.
+            df_trades[symbol].loc[diggity_day] = df_trades[symbol].loc[diggity_day] + 2000.
+            holdings = holdings + 2000.
+        if holdings == 0:
+            df_trades["Cash"].loc[diggity_day] = df_trades["Cash"].loc[diggity_day] + 1000. * prices_dataframe[symbol].loc[diggity_day] * -1.
+            df_trades[symbol].loc[diggity_day] = df_trades[symbol].loc[diggity_day] + 1000.
+            holdings = holdings + 1000.
+        return df_trades, holdings
+
+    def compute_trades_and_holdings_sell(self, df_trades, prices_dataframe, symbol, holdings, diggity_day):
+        if holdings == 1000:
+            df_trades["Cash"].loc[diggity_day] = df_trades["Cash"].loc[diggity_day] + 2000.0 * prices_dataframe[symbol].loc[diggity_day]
+            df_trades[symbol].loc[diggity_day] = df_trades[symbol].loc[diggity_day] - 2000.
+            holdings = holdings - 2000
+        if holdings == 0:
+            df_trades["Cash"].loc[diggity_day] = df_trades["Cash"].loc[diggity_day] + 1000. * prices_dataframe[symbol].loc[diggity_day]
+            df_trades[symbol].loc[diggity_day] = df_trades[symbol].loc[diggity_day] - 1000.0
+            holdings = holdings - 1000.
+
+        return df_trades, holdings
+
+    def compute_cumulative_return(self, prices, prices_dataframe, df_trades, sv):
+        holdings_df = pd.DataFrame(np.zeros(df_trades.shape), columns=df_trades.columns, index=df_trades.index)
+        values_df = holdings_df.copy()
+        first_diggity_day = prices.index[0]
+        holdings_df["Cash"].loc[first_diggity_day] = sv
+        holdings_df.loc[first_diggity_day] = holdings_df.loc[first_diggity_day] + df_trades.loc[first_diggity_day]
+        holdings_df = holdings_df.cumsum()
+        values_df = (holdings_df * prices_dataframe)
+        df_port_val = values_df.sum(axis=1)
+        cumulative_return = (df_port_val.iloc[-1] - sv) / sv
+        return cumulative_return
 
     # this method should create a QLearner, and train it for trading
     def addEvidence(self, symbol = "IBM", \
         sd=dt.datetime(2008,1,1), \
         ed=dt.datetime(2009,1,1), \
-        sv = 10000): 
+        sv = 10000):
 
         # add your code to do learning here
 
@@ -29,36 +159,103 @@ class StrategyLearner(object):
         prices = prices_all[syms]  # only portfolio symbols
         prices_SPY = prices_all['SPY']  # only SPY, for comparison later
         if self.verbose: print prices
-  
-        # example use with new colname 
+
+        # example use with new colname
         volume_all = ut.get_data(syms, dates, colname = "Volume")  # automatically adds SPY
         volume = volume_all[syms]  # only portfolio symbols
         volume_SPY = volume_all['SPY']  # only SPY, for comparison later
         if self.verbose: print volume
 
+        # feature engineering, get your new features for the state space.
+        state_bolli, state_bandwidth, state_momentum, state_rsi = self.add_indicators(prices, save_data=self.save_data, symb=symbol)
+
+        # compute the current state, well really all the states.
+        states = self.discretize(state_bolli, state_bandwidth, state_momentum, state_rsi)
+        number_of_bins = self.nbins
+        self.learner = QLearner(num_states=(number_of_bins ** 4), \
+                                num_actions = 3, \
+                                alpha = 0.5, \
+                                gamma = 0.9, \
+                                rar = 0.0, \
+                                radr = 0.0, \
+                                dyna = 0, \
+                                verbose = False)
+
+        converged = False
+        count = 0
+        converged_yet = 0.
+
+        while (not converged) and (count < 30):
+
+            total_reward = 0
+            prices_dataframe, df_trades = self.init_trades_and_prices_df(prices)
+            holdings = 0
+
+            for i in range(state_bolli.shape[0]):
+                current_state = states[i]
+                action = self.learner.querysetstate(current_state)
+                # first day
+                reward = self.compute_last_reward(prices, holdings, action, i)
+                # get the current state and the reward to get an action.
+                action = self.learner.query(current_state,reward)
+                diggity_day = prices.index[i]
+                if action == 0:
+                    df_trades, holdings = self.compute_trades_and_holdings_sell(df_trades, prices_dataframe, symbol, holdings, diggity_day)
+                if action == 1:
+                    df_trades, holdings = self.compute_trades_and_holdings_buy(df_trades, prices_dataframe, symbol, holdings, diggity_day)
+                total_reward += reward
+
+            cumulative_return = self.compute_cumulative_return(prices, prices_dataframe, df_trades, sv)
+            count += 1
+
+            if abs(converged_yet - cumulative_return) * 100. < 0.0001:
+                converged = True
+            else:
+                converged_yet = cumulative_return
+
+        return pd.DataFrame(df_trades[symbol])
     # this method should use the existing policy and test it against new data
     def testPolicy(self, symbol = "IBM", \
-        sd=dt.datetime(2009,1,1), \
-        ed=dt.datetime(2010,1,1), \
+        sd=dt.datetime(2008,1,1), \
+        ed=dt.datetime(2009,1,1), \
         sv = 10000):
 
         # here we build a fake set of trades
         # your code should return the same sort of data
         dates = pd.date_range(sd, ed)
         prices_all = ut.get_data([symbol], dates)  # automatically adds SPY
-        trades = prices_all[[symbol,]]  # only portfolio symbols
+        prices = prices_all[[symbol,]]  # only portfolio symbols
         trades_SPY = prices_all['SPY']  # only SPY, for comparison later
+        """
         trades.values[:,:] = 0 # set them all to nothing
         trades.values[0,:] = 1000 # add a BUY at the start
-        trades.values[40,:] = -1000 # add a SELL 
-        trades.values[41,:] = 1000 # add a BUY 
+        trades.values[40,:] = -1000 # add a SELL
+        trades.values[41,:] = 1000 # add a BUY
         trades.values[60,:] = -2000 # go short from long
         trades.values[61,:] = 2000 # go long from short
         trades.values[-1,:] = -1000 #exit on the last day
         if self.verbose: print type(trades) # it better be a DataFrame!
         if self.verbose: print trades
         if self.verbose: print prices_all
-        return trades
+        """
+        prices_dataframe, df_trades = self.init_trades_and_prices_df(prices)
+        holdings = 0
+        state_bolli, state_bandwidth, state_momentum, state_rsi = self.add_indicators(prices)
+        states = self.discretize(state_bolli, state_bandwidth, state_momentum, state_rsi)
+
+        for i in range(state_bolli.shape[0]):
+            current_state = states[i]
+            action = self.learner.querysetstate(current_state)
+            reward = self.compute_last_reward(prices, holdings, action, i)
+            diggity_day = prices.index[i]
+            if action == 0:
+                df_trades, holdings = self.compute_trades_and_holdings_sell(df_trades, prices_dataframe, symbol, holdings, diggity_day)
+            if action == 1:
+                df_trades, holdings = self.compute_trades_and_holdings_buy(df_trades, prices_dataframe, symbol, holdings, diggity_day)
+        return pd.DataFrame(df_trades[symbol])
 
 if __name__=="__main__":
     print "One does not simply think up a strategy"
+    learner = (StrategyLearner(save_data=True))
+    print(learner.addEvidence())
+    print(learner.testPolicy())
